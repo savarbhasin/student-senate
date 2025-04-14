9dff94aab9b8686ef2fe2aaa92f59f6b6af97290
Avi Avinav <aviavinav2004@gmail.com>
2025-04-14 12:11:55 +0530

    added write spreadsheet utility

--- lib/sheets.ts
+++ lib/sheets.ts
@@ -1,7 +1,7 @@
 import { google } from "googleapis";
 import keys from "@/creds.json" assert { type: "json" };
 
-export async function handler() {
+export async function readSpreadsheet(spreadsheetId: string, range: string) {
   const auth = await google.auth.getClient({
     projectId: keys.project_id,
     credentials: {
@@ -18,8 +18,40 @@ export async function handler() {
   const sheets = google.sheets({ version: "v4", auth });
 
   const data = await sheets.spreadsheets.values.get({
-    spreadsheetId: "1bWN4v6DJB0emsQUU5pydxRhIH6ibIERe3ujFBV5luEI",
-    range: "Sheet1!A:D",
+    spreadsheetId,
+    range,
   });
-  return data;
+  return data.data.values;
+}
+
+export async function writeSpreadsheet(
+  spreadsheetId: string,
+  range: string,
+  valueInputOption: string,
+  values: string[][]
+) {
+  const auth = await google.auth.getClient({
+    projectId: keys.project_id,
+    credentials: {
+      type: "service_account",
+      private_key: keys.private_key.split(String.raw`\n`).join("\n"),
+      client_email: keys.client_email,
+      client_id: keys.client_id,
+      token_url: keys.token_uri,
+      universe_domain: "googleapis.com",
+    },
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+  });
+
+  const sheets = google.sheets({ version: "v4", auth });
+
+  const data = await sheets.spreadsheets.values.update({
+    spreadsheetId,
+    range,
+    valueInputOption,
+    requestBody: {
+      values,
+    },
+  });
+  return data.status;
 }
